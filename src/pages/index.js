348f0467dbaf1f3155a6957acd8379c994bd126d
Scottiedog45 <scottiedog45@gmail.com>
2018-03-10 23:45:33 -0500

    styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,13 +27,22 @@ import FontAwesome from 'react-fontawesome';
 
 import {media} from '../style-utils'
 
-import {FaGithubSquare, FaEnvelope, FaLinkedinSquare} from 'react-icons/lib/fa'
+import {FaAngleDoubleDown, FaGithubSquare, FaEnvelope, FaLinkedinSquare} from 'react-icons/lib/fa'
+
+import Fade from 'react-reveal/Fade';
 
 const Button = styled.button`
-  border-color: black;
-  background-color: inherit;
-  border-style: solid;
-  border-width: 1px;
+  color: white;
+  background-color: #e94889;
+  border: none;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  cursor: pointer;
+  width: 18vw;
+  height: 7vh;
+  ${media.handheld`
+    width: 30vw;
+    `}
+
 `;
 
 const Panel = styled.div`
@@ -107,7 +116,7 @@ const Url = styled.a`
 
 const IconWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   margin-left: auto;
   margin-right: auto;
   display: flex;
@@ -116,7 +125,7 @@ const IconWrapper = styled.div`
 `;
 
 const Icon = styled.img`
-  height: 20px;
+  height: 47px;
 `;
 
 const Footer = styled.div`
@@ -178,6 +187,7 @@ const Canvas = styled.canvas`
 `;
 
 const ContactFloater = styled.div`
+  z-index: 3;
   font-size: 40px;
   display: flex;
   margin-top: 70px;
@@ -190,6 +200,10 @@ const ContactFloater = styled.div`
   bottom: 1vw;
 `;
 
+const ContactLink = styled.a`
+  color: inherit;
+`;
+
 const ContactIcon = styled.img`
   height: 50px;
   margin-left: 20px;
@@ -197,6 +211,18 @@ const ContactIcon = styled.img`
 
 const HeyThere = styled.h1`
   margin-bottom: 0px;
+`;
+
+const Card = styled.div`
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  padding: 23px 16px;
+  background-color: white;
+`;
+
+const Screenshot = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 `;
 
 const IndexPage = () => (
@@ -210,24 +236,45 @@ const IndexPage = () => (
       <ScrollAnimation>
         scroll
         <br />
+        <FaAngleDoubleDown />
+        <br />
         or
+        <br />
       </ScrollAnimation>
       <ButtonWrapper>
         <Scrollchor to='#work'>
           <Button>
-            See my work
+            see my work
           </Button>
         </Scrollchor>
+        <a href={'mailto:scottiedog45@gmail.com'}>
+        <Button>
+          get in touch
+        </Button>
+        </a>
       </ButtonWrapper>
       <ContactFloater>
+        <ContactLink href={'mailto:scottiedog45@gmail.com'}>
         <FaEnvelope />
+        </ContactLink>
+        <ContactLink href={'https://github.com/scottiedog45'}>
         <FaGithubSquare />
+        </ContactLink>
+        <ContactLink href={'https://www.linkedin.com/in/scott-o-toole-8b35b01b/'}>
         <FaLinkedinSquare />
+        </ContactLink>
       </ContactFloater>
     </TitlePanel>
     <Panel1>
+    <Card>
+      <Fade left>
+
       <HeyThere>Hey, there!</HeyThere>
+      <br />
       <h2>My goal is to connect people with beautiful solutions.</h2>
+      </Fade>
+      <Fade up>
+
       <Bio>
         I&#39;m a recent graduate of the Front End
         Web Development bootcamp at Thinkful. I love how web development
@@ -237,25 +284,38 @@ const IndexPage = () => (
         I&#39;m excited by innovation,
         and always driven to find solutions that are efficient and artistic.
         <br /> <br />
+        <b>Front-end:</b> React, Redux, Redux-Form, Jquery, Css, Html, Styled-components
+        <br />
+        Back-end: Node, Express
+        <br />
+        Testing: Mocha, Chai, Enzyme, Jest
+        <br /> <br />
         At some
         point I drank the CrossFit kool-aid, so if my computer is off you can also find
         me in a local gym racking up some burpees!
         <br /><br />
         Continue scrolling to see examples of my work.
       </Bio>
+
+      </Fade>
+      </Card>
     </Panel1>
     <hr />
     <Panel id={'work'}>
       <a id={'work'} />
-      <ProjectTitle>
-        Piqnic
-      </ProjectTitle>
-      <Url
+      <Card>
+      <Fade left>
+        <ProjectTitle>
+          Piqnic
+        </ProjectTitle>
+        <Url
       href={'https://diplomat-cat-50211.netlify.com/'}
       target={'_blank'}>
         www.piqnic.com
       </Url>
-      <img src={piqnicMac} alt={'screenshot of piqnic app'} />
+      </Fade>
+      <Fade up>
+      <Screenshot src={piqnicMac} alt={'screenshot of piqnic app'} />
       <IconWrapper>
         <Icon src={html} alt={'html logo'} />
         <Icon src={css} alt={'css logo'} />
@@ -265,9 +325,13 @@ const IndexPage = () => (
        <Description>
         An app that helps you quickly plan a picnic.
       </Description>
+      </Fade>
+      </Card>
     </Panel>
     <hr />
     <Panel>
+      <Card>
+      <Fade left>
       <ProjectTitle>
         hai·koo
       </ProjectTitle>
@@ -276,23 +340,29 @@ const IndexPage = () => (
         target={'_blank'}>
         www.haikoo.com
       </Url>
-      <img src={haikuMac} alt={'screenshot of Haiku app'} />
+      </Fade>
+      <Fade up>
+      <Screenshot src={haikuMac} alt={'screenshot of Haiku app'} />
       <IconWrapper>
         <Icon src={html} alt={'html logo'} />
         <Icon src={css} alt={'css logo'} />
         <Icon src={js} alt={'js logo'} />
-        <Icon src={jquery} alt={'jquery logo'} />
         <Icon src={mocha} alt={'mocha logo'} />
         <Icon src={chai} alt={'chai logo'} />
+        <Icon src={jquery} alt={'jquery logo'} />
         <Icon src={express} alt={'express logo'} />
         <Icon src={node} alt={'node logo'} />
       </IconWrapper>
       <Description>
         An app that syllable-checks your haiku, and let&#39;s you post and vote on other haikus!
       </Description>
+      </Fade>
+      </Card>
     </Panel>
     <hr />
     <Panel>
+    <Card>
+    <Fade left>
       <ProjectTitle>
         Leev
       </ProjectTitle>
@@ -301,7 +371,9 @@ const IndexPage = () => (
         target={'_blank'}>
         www.leev.com
       </Url>
-      <img src={leevMac} alt={'screenshot of leev app'} />
+      </Fade>
+      <Fade up>
+      <Screenshot src={leevMac} alt={'screenshot of leev app'} />
       <IconWrapper>
         <Icon src={html} alt={'html logo'} />
         <Icon src={styledcomponents} alt={'styled components logo'} />
@@ -318,6 +390,8 @@ const IndexPage = () => (
       <Description>
         Leev uses a simple interface to help you track your leave data.
       </Description>
+      </Fade>
+      </Card>
     </Panel>
   </div>
 )
